005ddf9bccd4627659586a129dbd7338e3b46eeb
FranMa <fmandia1201@gmail.com>
2024-11-03 00:20:10 +0100

    First proyect version

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, useState } from 'react-native';
+import Appbar from './src/screen/Appbar';
 
 export default function App() {
+  
+  const [inScreen, setInScreen] = useState(["TASK" | "COMPLETE TASK"]);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>LISTA</Text>
+      <Appbar inScreen={inScreen} setInScreen={setInScreen}></Appbar>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +20,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
   },
+  titulo: {
+    fontSize: 30,    
+  }
 });
---
+++ src/screen/Appbar.js
@@ -0,0 +1,25 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function ({inScreen, setInScreen}) {
+  
+  function select(inScreen) {
+    
+  } 
+  
+  return (
+    <View>
+        <TouchableOpacity>
+            <Text>
+
+            </Text>
+        </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    button: {
+
+    }
+});
